99412853d919237f9ea07302c71236e74591f6b8
PanarinSE <pseesp@mail.ru>
2020-09-01 22:29:46 +0300

    Added scan *.mp3 in current dir when program start.

--- SoundAlpha.py
+++ SoundAlpha.py
@@ -1,13 +1,9 @@
-from os import environ
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 from PyQt5 import QtWidgets
 import sys
 from SoundForm import Ui_MainWindow
-
-#pygame.mixer.music.load("alphavillesounds_like_a_melody.mp3")
-#pygame.mixer.music.load("Vechnye_Hity-Alphaville_-_Sounds_Like_A_Melody.mp3")
-
 
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -19,9 +15,11 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.btnClicked2)
         self.ui.pushButton_3.clicked.connect(self.btnClicked3)
         self.ui.pushButton_2.setEnabled(False)
-        self.ui.comboBox.addItem("MID_alphavillesounds_like_a_melody.mp3")
-        self.ui.comboBox.addItem("ORG_Alphaville_Sounds_Like_A_Melody.mp3")
-        self.ui.comboBox.setCurrentText("MID_alphavillesounds_like_a_melody.mp3")
+        for file in os.listdir(os.curdir):
+            if file.endswith(".mp3"):
+                self.ui.comboBox.addItem(file)
+                #print(file)
+        self.ui.comboBox.setCurrentText(file)
         self.ui.horizontalSlider.setMaximum(100)
         self.ui.horizontalSlider.setMinimum(0)
         self.ui.horizontalSlider.setSingleStep(10)
